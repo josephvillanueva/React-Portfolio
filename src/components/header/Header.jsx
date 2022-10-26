59f5e97e7fadd12bc80e931dbcf228c9f542b60a
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-10-27 03:06:21 +0800

    feat: Finished Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import Headersocials from './Headersocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Joseph Villanueva</h1>
+        <h5 className="text-light">Full-Stack Developer</h5>
+        <CTA/>
+        <Headersocials/>
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
