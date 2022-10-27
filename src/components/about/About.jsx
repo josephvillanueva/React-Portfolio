03c1da51bcb12fe1f0327389f1ececb931770ba3
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-10-27 21:25:40 +0800

    feat: about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>1 year working experience</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Previous experience</h5>
+              <small>Fontend Web Developer and Junior Cluod Engineer</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>Blockchain and Optical Character Recognition</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut in
+            tenetur maiores quo ducimus animi reprehenderit est et, quibusdam
+            delectus aperiam minima a magni laudantium ea ullam impedit atque
+            consequatur!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
